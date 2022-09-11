2189647caa08d7e4234d7554f7b449f9aa20d76c
Gustavo Linhaus <gustavo_linhaus@hotmail.com>
2022-09-11 09:44:14 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
+import { toast } from 'react-toastify';
 import './styles.css';
 
 type Props = {
@@ -9,7 +10,7 @@ type Props = {
 function handleClick(id: number) {
    axios(`${BASE_URL}/sales/${id}/notification`)
    .then(response => {
-      console.log("SUCESSO");
+      toast.info("SMS enviado com sucesso");
    });
 }
 function NotificationButton( {saleId} : Props) {
